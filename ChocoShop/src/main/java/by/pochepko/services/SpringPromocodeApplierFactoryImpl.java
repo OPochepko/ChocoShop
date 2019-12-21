0e2555a4cb1ce56da517803ebf97ce6c1fe252b7
pochepko_oleg <pochepkooleg@gmail.com>
2019-12-21 22:58:49 +0300

    add first javadoc for SpringPromocodeApplierFactoryIpl сори фор май ингилиш

--- ChocoShop/src/main/java/by/pochepko/services/SpringPromocodeApplierFactoryImpl.java
+++ ChocoShop/src/main/java/by/pochepko/services/SpringPromocodeApplierFactoryImpl.java
@@ -7,15 +7,32 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
+/**
+ * Implementation of {@link PromocodeApplierFactory} and {@link ApplicationContextAware}: gives implementation
+ * of {@link PromocodeApplier} that suitable for given promocode.
+ * Automatically search for {@link PromocodeApplier} beans in ApplicationContex by promocode.
+ * Bean name of {@link PromocodeApplier} bean should start from simple class name of given promocode with suffix
+ * "Applier" and first char should be in lower case. For example {@link PromocodeApplier} bean name for ExamplePromocode.class should be
+ * "examplePromocodeApplier. This meet the default Spring bean name for ExamplePromocodeApplier.class.
+ *
+ * @author Oleg Pochepko
+ */
+
 @Service
 public class SpringPromocodeApplierFactoryImpl implements PromocodeApplierFactory, ApplicationContextAware {
-
+    /** ApplicationContext that this object runs in */
     private ApplicationContext context;
-
+    /** Promocode to apply*/
     private Promocode promocode;
+    /** Service to excess data base*/
     @Autowired
     private DBService dbService;
 
+    /**
+     *
+     * @param code - code of {@link Promocode} to get suitable PromocodeApplier bean for
+     * @return PromocodeApplier bean for this promocode
+     */
     @Override
     public PromocodeApplier getPromocodeApplier(String code) {
 
@@ -28,11 +45,19 @@ public class SpringPromocodeApplierFactoryImpl implements PromocodeApplierFactor
 
     }
 
+    /**
+     * Set ApplicationContext that this object runs in
+     * @throws BeansException
+     */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         context = applicationContext;
     }
 
+    /**
+     * @param promocode - promocode
+     * @return name of Promocode bean suitable to apply promocode
+     */
     private String getPromocodeApplierBeanNameFromPromocode(Promocode promocode) {
         String promocodeClassName = promocode.getClass().getSimpleName();
         String defaultPromocodeApplierBeanName = Character.toLowerCase(promocodeClassName.charAt(0))
