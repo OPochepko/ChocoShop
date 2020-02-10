668d63ab87c80ccadb9e5345eab9aed4cd259538
pochepko_oleg <pochepkooleg@gmail.com>
2020-02-10 16:49:53 +0300

    add @PreAuthorize annotation for RestController methods instead of HttpSecurity

--- ChocoShop/src/main/java/by/pochepko/springcfg/SpringSecurityConfiguration.java
+++ ChocoShop/src/main/java/by/pochepko/springcfg/SpringSecurityConfiguration.java
@@ -1,27 +1,31 @@
 package by.pochepko.springcfg;
 
 import by.pochepko.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     public SpringSecurityConfiguration(UserService userService) {
         this.userService = userService;
     }
 
+
+    @Autowired
+    private final UserService userService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    private final UserService userService;
 
 
     @Override
@@ -33,12 +37,11 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.GET, "/chocolates/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.POST, "/chocolates/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/chocolates/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.GET, "/chocolates").hasAuthority("ADMIN")
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
                 .and()
-                .csrf().disable()
-                .formLogin().disable();
+                .formLogin().and()
+                .httpBasic();
     }
 }
